1a97f3ffe53fe367cbc66e1f3f7a80e7b68e8569
Daniel Enriquez <daniel.enm17@gmail.com>
2022-04-05 19:50:56 -0500

    yarn test passed

--- React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ React Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,18 @@
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+  const vowels = ["A", "E", "I", "O", "U", " "];
+  const finalWord = [];
+
+  for (let i = 0; i < word.length; i += 1) {
+    if (vowels.includes(word[i].toUpperCase()) || i === word.length - 1) {
+      finalWord.push(word[i]);
+    } else if (vowels.includes(word[i + 1].toUpperCase())) {
+      finalWord.push(word[i]);
+    } else {
+      finalWord.push(`${word[i]}-`);
+    }
+  }
+  return finalWord.join('');
 }
 
 module.exports = insertDash;
--- React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ React Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,78 @@
-function occurrences(text, word) {
-  return null;
+// TEXT AND WORD TO UPCASE
+// DEFINE COUNTER
+// SORT THE, SELECTION SORT O(n^2) | O(n^2) [QUICKSORT USING SORT USES (n log n)]
+/// DEFINE SWAP, INDEX_OF_MINIMUM, AND SELECTION SORT FUNCTIONS
+// BINARY SEARCH (FIND BY FIRST LETTER) Complexity: O(log n) | O(1)
+// CHECK IF LETTER FOUND === WORD, IF IT DOES, COUTER += 1
+
+function occurrences(text, targetValue) {
+  const uppcasedText = text.toUpperCase();
+  const word = targetValue.toUpperCase();
+  const array = uppcasedText.split(" ");
+  let counter = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === word) {
+      counter += 1;
+    }
+  }
+
+  return counter;
+  // const swap = (firstIndex, secondIndex) => {
+  //   const temp = array[firstIndex];
+  //   array[firstIndex] = array[secondIndex];
+  //   array[secondIndex] = temp;
+  // };
+
+  // const indexOfMinimum = (startIndex) => {
+  //   let minValue = array[startIndex];
+  //   let minIndex = startIndex;
+
+  //   for (let i = minIndex + 1; i < array.length; i += 1) {
+  //     if (array[i] < minValue) {
+  //       minIndex = i;
+  //       minValue = array[i];
+  //     }
+  //   }
+  //   return minIndex;
+  // };
+
+  // const selectionSort = () => {
+  //   let value;
+  //   for (let i = 0; i < array.length; i += 1) {
+  //     value = indexOfMinimum(i);
+  //     swap(i, value);
+  //   }
+  //   return array;
+  // };
+
+  // const binarySearch = () => {
+  //   // Define min and max values
+  //   let min = 0;
+  //   let max = array.length - 1;
+  //   let guess;
+  //   while (max >= min) {
+  //     guess = Math.floor((max + min) / 2);
+  //     if (array[guess] === word) {
+  //       counter += 1;
+  //     }
+
+  //     if (array[guess] < word) {
+  //       min = guess + 1;
+  //     } else {
+  //       max = guess - 1;
+  //     }
+  //   }
+  //   return guess;
+  // };
+
+  // selectionSort();
+  // binarySearch();
+
+  // console.log(word);
+  // console.log(array);
+  // console.log(counter);
 }
 
+occurrences("a fat cat sat on a mat and ate a fat rat", "fat"); // => 2
 module.exports = occurrences;
